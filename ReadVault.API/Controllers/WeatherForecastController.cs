8e6010e48dc139b9116e47469c8bfed84356bb9f
Yakup Budak <y.budak@mailbox.org>
2024-08-15 23:27:27 +0200

    Projektdateien hinzufügen.

---
+++ ReadVault.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReadVault.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Made call to Weather Endpoint");
+            try
+            {
+                throw new Exception("This is our logging test exception");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex , "Fatal Giga Error Occurred");
+                throw;
+            }
+            
+        }
+    }
+}
